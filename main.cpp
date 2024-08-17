aaa9185b507baf32b496ddf2367a3a5134cbf87d
Ayoub <itsayoubkheyar06@gmail.com>
2024-08-17 10:46:41 +0100

    pattern 5

--- main.cpp
+++ main.cpp
@@ -2,10 +2,49 @@
 
 using namespace std;
 
+void drawSpaces(int quantity)
+{
+    for (int i = 1; i <= quantity; i++)
+    {
+        cout << " ";
+    }
+}
+
+void drawStarsLine(int quantity)
+{
+    for (int i = 1; i <= quantity; i++)
+    {
+        cout << "*";
+    }
+}
+
 int main()
 {
+    int size = 10;
 
-    cout << "hello world";
+    cout << "pattern 5 : \n";
+
+    for (int i = 1; i <= size; i += 2)
+    {
+        drawSpaces((size - i) / 2);
+
+        drawStarsLine(i);
+
+        // drawSpaces((size - i) / 2);
+
+        cout << "\n";
+    }
+
+    for (int i = size - 3; i >= 1; i -= 2)
+    {
+        drawSpaces((size - i) / 2);
+
+        drawStarsLine(i);
+
+        // drawSpaces((size - i) / 2);
+
+        cout << "\n";
+    }
 
     return 0;
 }
